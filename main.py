051cb28edd9eec46a25df26e87aac29f185f5c4d
Ugur Doga Sezgin <doga.sezgin@gmail.com>
2026-02-18 14:07:43 +0000

    add: forming and drawing a math expression

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello")
+from value import Value
+from draw_helper import draw_dot
+
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a * b; e.label = 'e'
+d = e + c; d.label = 'd'
+f = Value(-2.0, label='f')
+L = d * f; L.label = 'L'
+
+draw_dot(L).view()
---
+++ value.py
@@ -0,0 +1,15 @@
+class Value:
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        return Value(self.data + other.data, (self, other), '+')
+
+    def __mul__(self, other):
+        return Value(self.data * other.data, (self, other), '*')
