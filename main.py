ad0f62acfcbedd8a37cb23e1b8df32eb46eb8eb0
Ugur Doga Sezgin <doga.sezgin@gmail.com>
2026-02-18 14:53:56 +0000

    add: simple neural network nodes

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from value import Value
 from draw_helper import draw_dot
 
-a = Value(2.0, label='a')
-b = Value(-3.0, label='b')
-c = Value(10.0, label='c')
-e = a * b; e.label = 'e'
-d = e + c; d.label = 'd'
-f = Value(-2.0, label='f')
-L = d * f; L.label = 'L'
+def make_nodes():
+    a = Value(2.0, label='a')
+    b = Value(-3.0, label='b')
+    c = Value(10.0, label='c')
+    e = a * b; e.label = 'e'
+    d = e + c; d.label = 'd'
+    f = Value(-2.0, label='f')
+    L = d * f; L.label = 'L'
+    draw_dot(L).view()
 
-draw_dot(L).view()
+def make_neurons():
+    x1 = Value(2.0, label='x1')
+    x2 = Value(0.0, label='x2')
+    w1 = Value(-3.0, label='w1')
+    w2 = Value(1.0, label='w2')
+    b = Value(6.8813735870195432, label='b')
+    x1w1 = x1*w1; x1w1.label = 'x1w1'
+    x2w2 = x2*w2; x2w2.label = 'x2w2'
+    x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1w1 + x2w2'
+    n = x1w1x2w2 + b; n.label = 'n'
+    o = n.tanh(); o.label = 'o'
+
+    o.grad = 1.0
+    n.grad = 1 - o.data**2
+    b.grad = n.grad
+    x1w1x2w2.grad = n.grad
+    x1w1.grad = x1w1x2w2.grad
+    x2w2.grad = x1w1x2w2.grad
+    x2.grad = x2w2.grad * w2.data
+    w2.grad = x2w2.grad * x2.data
+    x1.grad = x1w1.grad * w1.data
+    w1.grad = x1w1.grad * x1.data
+
+    draw_dot(o).view()
+
+make_neurons()
--- value.py
+++ value.py
@@ -1,6 +1,9 @@
+import math
+
 class Value:
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
+        self.grad = 0.0
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -13,3 +16,8 @@ class Value:
 
     def __mul__(self, other):
         return Value(self.data * other.data, (self, other), '*')
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        return Value(t, (self,), 'tanh')
