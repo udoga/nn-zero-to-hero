e99dabc18d33fd673ca004449535097d85192f74
Ugur Doga Sezgin <doga.sezgin@gmail.com>
2026-02-24 22:35:05 +0000

    add: predicting next char using a neural network

--- makemore/main.py
+++ makemore/main.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 def get_word_chars(word):
@@ -66,6 +67,24 @@ def get_avg_neg_log_likelihood(prob_matrix, words, all_chars):
             total_count += 1
     return total_nll / total_count
 
+def train_neural_network(bigrams, all_chars):
+    xs = torch.tensor([all_chars.index(c1) for c1, _ in bigrams])
+    ys = torch.tensor([all_chars.index(c2) for _, c2 in bigrams])
+    x_encoded = F.one_hot(xs, num_classes=len(all_chars)).float()
+    y_encoded = F.one_hot(ys, num_classes=len(all_chars)).float()
+
+    g = torch.Generator().manual_seed(2147483647)
+    W = torch.randn((len(all_chars), len(all_chars)), generator=g, requires_grad=True)
+
+    for _ in range(100):
+        logits = x_encoded @ W
+        probs = F.softmax(logits, dim=1)
+        loss = -probs[torch.arange(len(xs)), ys].log().mean()
+        print(loss.item())
+        W.grad = None
+        loss.backward()
+        W.data += -50 * W.grad  # type: ignore
+
 words = open("names.txt", 'r').read().splitlines()
 all_chars = get_all_chars(words)
 bigrams = get_all_bigrams(words)
@@ -74,4 +93,5 @@ count_matrix = get_bigram_matrix(bigrams, all_chars) + 1 # smoothing
 prob_matrix = count_matrix.float() / count_matrix.sum(dim=1, keepdim=True)
 # show_bigram_matrix(prob_matrix, all_chars)
 # print(get_sample_names(prob_matrix, all_chars, 10))
-print(get_avg_neg_log_likelihood(prob_matrix, ["andrejq"], all_chars))
+# print(get_avg_neg_log_likelihood(prob_matrix, words, all_chars))
+train_neural_network(bigrams, all_chars)
